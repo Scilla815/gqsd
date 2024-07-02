bda5c70ea0c27ab1f9e35021404af9a0103995d4
Gary Luo <garyluo815@gmail.com>
2024-07-02 16:45:14 -0700

    Added user inputs to make a simple user interface

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 from golflive import processExcel, addRanking
 from firestore import initializeDatabaseClient, writeToFirestore, generateCSV
+import inquirer
 
-db = initializeDatabaseClient()
+def main():
+    db = initializeDatabaseClient()
 
-df = processExcel('data/match_score_3.xlsx')
-df = addRanking(df)
+    while True:
+        questions = [
+        inquirer.List('action',
+                        message="What would you like to do?",
+                        choices=["Process Scores", "Update Users", "Generate CSV", "Exit"],
+                    ),
+        ]
+        answers = inquirer.prompt(questions)
+        userInput = answers["action"]
+        match userInput:
+            case "Process Scores":
+                filename = input("Which match do you want to process? ")
+                df = processExcel("data/" + filename)
+                df = addRanking(df)
+                writeToFirestore(db, df)
+            case "Update Users":
+                return 1
+            case "Generate CSV":
+                generateCSV(db)
+            case "Exit":
+                return
+            case _:
+                print("Invalid Input. Please try again")
 
-print(df)
-    
-# writeToFirestore(db, df)
-
-print(generateCSV(db))
-
-# print(db)
+if __name__ == "__main__":
+    print(main())
