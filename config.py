22ce3facd954b4405389f7d9b32e2987e0804f06
Gary Luo <garyluo815@gmail.com>
2024-07-07 01:41:14 -0700

    Deleted keys

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-CRED_PATH = "gqsd-91223-firebase-adminsdk-tqw24-12f3780010.json"
+CRED_PATH = "gqsd-91223-firebase-adminsdk-tqw24-0b8fffc221.json"
 OUTPUT_PATH = "output/output.csv"
 MEMBERS_PATH = "data/membership.xlsx"
